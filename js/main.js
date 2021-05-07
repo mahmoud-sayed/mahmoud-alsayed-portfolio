bc83f80a6ab59408da0106eeda2c7bc3487e52b4
Mahmoud Alsayed <you@example.com>
2021-05-07 15:30:15 +0200

    working responcive website but need some work on cards

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ window.addEventListener('scroll', () => {
     let nav = document.querySelector('nav');
     nav.classList.toggle('sticky', window.scrollY > 0);
 });
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navBarList = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navBarList.classList.toggle('active')
+});
